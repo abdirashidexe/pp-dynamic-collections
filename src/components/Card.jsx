93f3b6d3af7192053cb1e61949af03ed2f90a3cd
rjfredr <fredrickalexander1727@gmail.com>
2025-10-09 13:26:30 -0700

    css completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,17 @@ export default function Playercard({player}) {
     return(
         <div className="playercard">
             <img src={player.img}></img>
-            <h1>{player.jersey}</h1>
-            <h1>{player.name}</h1>
+            <h1 id="number">#{player.jersey}</h1>
+            <h2 id="pos">{player.position}</h2>
+            <h1 id="name">{player.name}</h1>
             <div className="playerinfo">
-                <h2>{player.birthdate}</h2>
-                <h2>Former teams:</h2>
-                {player.teams.map((item, index)=> (
-                    <h3 key={index}>{item}</h3>
-                ))}
+                <div className="teams">
+                    {player.teams.map((item, index)=> (
+                        <h4 key={index}>{item}</h4>
+                    ))}
+                </div>
             </div>
-            <h2>{player.position}</h2>
-            <h2>{player.stat}</h2>
+            <h2 id="stat">{player.stat}</h2>
         </div>
     )
 }
